0bf28c7b25e5cb418c58fe4b187ce94f54e4835b
sandesh6636 <134988883+sandesh6636@users.noreply.github.com>
2024-05-02 12:27:19 +0000

    Array basic 2nd lerned

---
+++ 02_basicJs/02arrayBasic.js
@@ -0,0 +1,27 @@
+const mySathi = ["me", "myself", "sandesh"];
+const hisSathi = ["him", "himself", "kushal"];
+
+// mySathi.push(hisSathi);
+// console.log(mySathi[3][2]);
+
+// const newArray = mySathi.concat(hisSathi);
+// console.log(newArray);
+
+/* spread operator */
+
+// const allSathii = [...mySathi, ...hisSathi];
+// console.log(allSathii);
+
+// const newArray4 = [1, 2, [32, 3], [233, 2323, [3, 333, 666]]];
+// const flatArray = newArray4.flat(Infinity);
+// console.log(flatArray);
+
+console.log(Array.isArray(mySathi));
+console.log(Array.from("Sandeh"));
+console.log(Array.from({ name: "Kushal" })); //interesting --> gives an empty array
+
+let score1 = 333;
+let score2 = 349;
+let score3 = 800;
+
+console.log(Array.of(score1, score2, score3));
